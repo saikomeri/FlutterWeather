5e6b0fabbb7376b614f9f2d9227b47284cf93d5b
Sai Komeri <saisampurna995@gmail.com>
2025-11-28 19:45:00 -0500

    set up main entry point

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_weather/core/theme/app_theme.dart';
+import 'package:flutter_weather/presentation/providers/weather_provider.dart';
+import 'package:flutter_weather/presentation/screens/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const ProviderScope(child: WeatherApp()));
+}
+
+class WeatherApp extends ConsumerWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(isDarkModeProvider);
+
+    return MaterialApp(
+      title: 'Flutter Weather',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: const HomeScreen(),
+    );
+  }
+}
